cef702dadffde4ad6ffd9cda6af25e342e672a04
yangchongyang.york <yangchongyang.york@shandagames.com>
2017-09-25 15:32:13 +0800

    Initial commit

---
+++ src/scalaHttp/scalaHttpServerDemo.scala
@@ -0,0 +1,17 @@
+package scalaHttp
+
+/**
+  * Created by Administrator on 2017/9/25.
+  */
+class scalaHttpServerDemo {
+
+}
+
+
+object scalaHttpServerDemo{
+
+
+  def main(args: Array[String]): Unit = {
+    print("3")
+  }
+}
